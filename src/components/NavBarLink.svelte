151766d10a5de53e8e828a36e2976720935d9180
emoltz <es5541@nyu.edu>
2023-07-24 20:44:40 -0400

    basic setup

---
+++ src/components/NavBarLink.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let href: string;
+    export let text: string;
+
+</script>
+
+<li class="mr-6">
+    <a class="text-white hover:text-blue-800" href="{href}">
+      <button class="p-3">
+        {text}
+      </button>
+      
+  
+  </a>
+  </li>
