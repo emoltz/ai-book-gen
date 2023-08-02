919869cc67aef9ee8d5ec45de24746a62ef77ba5
connor miller <checkthegate21@gmail.com>
2023-08-02 10:14:21 -0400

    Some initial designs and layout.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,39 @@
-<div class="text-3xl font-bold underline">
-    AI Book Generation
-</div>
+<script></script>
 
-<div class="text-amber-600 hover:text-black">
-    Making an AI book generator. Have you heard of <a href="https://nanogenmo.github.io/" target="_blank">NaNoGenMo</a>?
+<div class="m-10">
+	<div class="text-3xl font-bold underline mb-5">AI Book Generation</div>
+
+	<div class="text-amber-600">
+		Making an AI book generator, inspired by <a href="https://nanogenmo.github.io/" target="_blank"
+			>NaNoGenMo</a
+		>. Here, we will generate the first page of a novel and see if you like it. Then you can share
+		your novel with other people and rank which ones should be made into a full text.
+	</div>
+	<div class="mt-5">
+		<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+			Generate Novel
+		</button>
+    <div class="mt-5">Loading...</div>
+	</div>
+	<div class="text-3xl font-bold underline mt-5">Browse Creations</div>
+	<div class="text-amber-600 mt-5">
+		Here you can check out stories that other users have generated.
+	</div>
+	{#each { length: 3 } as story}
+		<div>
+			<div class="mt-5">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+				voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+				cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+            <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mt-3">
+                Upvote
+              </button>
+              <button class="bg-orange-300 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mt-3">
+                Downvote
+              </button>
+		</div>
+	{/each}
 </div>
