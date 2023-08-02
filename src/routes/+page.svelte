1a81be29a23f3c90226c06e29dd7e913653c0e74
connor miller <checkthegate21@gmail.com>
2023-08-02 09:47:13 -0400

    Connor's first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="text-3xl font-bold underline">
-    Testing!
+    AI Book Generation
 </div>
 
 <div class="text-amber-600 hover:text-black">
-    Another test!
+    Making an AI book generator. Have you heard of <a href="https://nanogenmo.github.io/" target="_blank">NaNoGenMo</a>?
 </div>
