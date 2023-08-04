a3c61a9e7d62214be03628995604319f5dca68bd
Ethan Shafran Moltz <33405530+emoltz@users.noreply.github.com>
2023-08-04 23:31:06 +0000

    modifications to textarea component

--- src/components/TextArea.svelte
+++ src/components/TextArea.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-     let value:string = "";
+     export let value:string = "";
+     export let placeholder: string = "";
 </script>
 
 <div class="p-3">
     <textarea 
     bind:value={value}
     class=" w-full px-3 py-2 text-sm text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline"
-    placeholder="Type your prompt here..."
+    placeholder={placeholder}
   />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import TextArea from "../components/TextArea.svelte";
-
+	let value: string = ""
 
 </script>
 
-<TextArea value="Hello World!"/>
+<TextArea 
+	value={value}
+	placeholder="Type your prompt here..."
+/>
